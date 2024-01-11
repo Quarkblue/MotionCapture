82cb3a504b6be4cbddb45ee625ac82273a98c1bc
IshanTrivedi13 <1313inferno1313@gmail.com>
2024-01-11 12:42:07 +0530

    score and timer

--- MotionCapture/Assets/Scripts/ScoringSystem.cs
+++ MotionCapture/Assets/Scripts/ScoringSystem.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoringSystem : MonoBehaviour
 {
     public int score = 0;
+    public float timer = 0;
+    [SerializeField]
+    public TextMeshProUGUI text;
+    [SerializeField]
+    public TextMeshProUGUI timerText;
 
     private void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "veg")
         {
             score++;
-            Debug.Log(score);
+            text.text = $"Score: {score}";
         }
+    }
+    private void Update() 
+    {
+        timer += Time.deltaTime;
+       
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+
+
+        timerText.text = $"Timer: {niceTime}";
     }
 }
